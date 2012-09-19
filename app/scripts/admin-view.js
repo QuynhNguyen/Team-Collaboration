fb2f5a0f713c67ab0e7271ec14c77722d434bd50
Quynh Nguyen <quynhnguyen003@gmail.com>
2012-09-18 19:59:25 -0700

    add create project func for frontend

--- app/scripts/admin-view.js
+++ app/scripts/admin-view.js
@@ -16,7 +16,19 @@ TeamCollaboration.AdminSideBar = Backbone.View.extend({
 });
 
 TeamCollaboration.AdminProjectManagementMain = Backbone.View.extend({
+  events: {
+    "click button": "createProject"
+  },
   template: _.template($('#tpl-admin-project-management-main').html()),
+  createProject: function() {
+    var projectName;
+    projectName = $('#projectName').val();
+    console.log(projectName);
+    this.model = new TeamCollaboration.ProjectModel({
+      name: projectName
+    });
+    return console.log(this.model);
+  },
   render: function(e) {
     this.$el.html(this.template());
     return this;
--- app/scripts/views/admin-view.coffee
+++ app/scripts/views/admin-view.coffee
@@ -18,8 +18,17 @@ TeamCollaboration.AdminSideBar = Backbone.View.extend(
 )
 
 TeamCollaboration.AdminProjectManagementMain = Backbone.View.extend(
+
+	events: 
+		"click button": "createProject"
 	
 	template: _.template($('#tpl-admin-project-management-main').html())
+	
+	createProject: ->
+		projectName = $('#projectName').val()
+		console.log(projectName)
+		this.model = new TeamCollaboration.ProjectModel({name:projectName})
+		console.log(this.model)
 	
 	render: (e) ->
 		this.$el.html(this.template())
