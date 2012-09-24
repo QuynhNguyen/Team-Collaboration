18a08d55f0c4ca04d34d6b83821da3314012c8f4
Quynh Nguyen <quynhnguyen003@gmail.com>
2012-09-23 20:31:02 -0700

    remove local npm-debug log and added it to gitignore

---
+++ app/scripts/templates/misc/tpl-project-deleted.coffee
@@ -0,0 +1,9 @@
+document.write """
+	<script type="text/template" id="tpl-project-deleted">
+		<div class="alert alert-success span4">
+			Succesfully Deleted Project
+		<br /><br />
+		<p><a href="/#admin/project-management" class="btn btn-primary" id="project-deleted">Create Another Project</a></p>
+		</div>
+	</script>
+"""
---
+++ app/scripts/templates/misc/tpl-project-deleted.js
@@ -0,0 +1,2 @@
+
+document.write("<script type=\"text/template\" id=\"tpl-project-deleted\">\n	<div class=\"alert alert-success span4\">\n		Succesfully Deleted Project\n	<br /><br />\n	<p><a href=\"/#admin/project-management\" class=\"btn btn-primary\" id=\"project-deleted\">Create Another Project</a></p>\n	</div>\n</script>");
